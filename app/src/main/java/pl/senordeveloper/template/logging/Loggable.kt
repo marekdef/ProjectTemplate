ca0a421005da7e2838acda3330672228d8e157e1
Marek Defecinski <marekdef@gmail.com>
2022-07-26 21:44:47 +0200

    Small changes to the Loggable interface

--- app/src/main/java/pl/senordeveloper/template/logging/Loggable.kt
+++ app/src/main/java/pl/senordeveloper/template/logging/Loggable.kt
@@ -4,29 +4,25 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 interface Loggable {
-    var LOGGER: Logger
+    val logger: Logger
     val identityCode: String
     get() = "0x${System.identityHashCode(this).toString(16)}"
 
-    fun trace(format: String, vararg args: Any) = LOGGER.trace(format, args)
-    fun trace(format: String, throwable: Throwable) = LOGGER.trace(format, throwable)
-    fun debug(format: String, vararg args: Any) = LOGGER.debug(format, args)
-    fun debug(format: String, throwable: Throwable) = LOGGER.debug(format, throwable)
-    fun info(format: String, vararg args: Any) = LOGGER.info(format, args)
-    fun info(format: String, throwable: Throwable) = LOGGER.info(format, throwable)
-    fun warn(format: String, vararg args: Any) = LOGGER.warn(format, args)
-    fun warn(format: String, throwable: Throwable) = LOGGER.warn(format, throwable)
-    fun error(format: String, vararg args: Any) = LOGGER.error(format, args)
-    fun error(format: String, throwable: Throwable) = LOGGER.error(format, throwable)
+    fun trace(format: String, vararg args: Any) = logger.trace(format, args)
+    fun trace(format: String, throwable: Throwable) = logger.trace(format, throwable)
+    fun debug(format: String, vararg args: Any) = logger.debug(format, args)
+    fun debug(format: String, throwable: Throwable) = logger.debug(format, throwable)
+    fun info(format: String, vararg args: Any) = logger.info(format, args)
+    fun info(format: String, throwable: Throwable) = logger.info(format, throwable)
+    fun warn(format: String, vararg args: Any) = logger.warn(format, args)
+    fun warn(format: String, throwable: Throwable) = logger.warn(format, throwable)
+    fun error(format: String, vararg args: Any) = logger.error(format, args)
+    fun error(format: String, throwable: Throwable) = logger.error(format, throwable)
 
 
     companion object {
         inline fun <reified T> delegate(): Loggable = object: Loggable {
-            override var LOGGER: Logger = LoggerFactory.getLogger(T::class.java)
-
-            override fun debug(format: String, vararg args: Any) {
-                super.debug(format, *args)
-            }
+            override val logger: Logger = LoggerFactory.getLogger(T::class.java)
         }
     }
 }
